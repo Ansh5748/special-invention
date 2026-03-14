13eaec86d0858a1900d00d31cfeb441d48412933
Divyansh Gupta <divyanshgupta5748@gmail.com>
2026-03-14 19:18:11 +0530

    third commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,15 +5,7 @@ const About = () => {
     <div className="about-section" id="about">
       <div className="about-me">
         <h3 className="title">About Me</h3>
-        <p className="para"
-        style={{
-        fontSize: "1.05rem",
-        lineHeight: "1.8",
-        color: "#ffffff",
-        maxWidth: "750px",
-        margin: "0 auto",
-        wordSpacing: "2px"
-      }}>
+        <p className="para">
           Versatile Full Stack Developer with a strong foundation in building end-to-end web and mobile applications, scalable backend systems, and cloud-native infrastructure. 
           Experienced in both frontend and backend development, mobile application development, cloud platforms, and DevOps practices. 
           Contributed to open-source projects in the cybersecurity domain, applying practical knowledge to enhance system security, automation, and performance monitoring. 
